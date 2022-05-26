fdaf191cf652f8abc2bd347310bd766f9c6cc5b0
youngeun choi <choi46910@naver.com>
2022-05-26 22:58:33 +0900

    22-05-26 리액트 학습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import { people } from './data.js';
-import { getImageUrl } from './utils.js';
+import { useState, useEffect } from 'react';
+import Clock from './Clock.js';
 
-export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
+}
+
+export default function App() {
+  const time = useTime();
+  const [color, setColor] = useState('lightcoral');
   return (
-    <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
-    </article>
+    <div>
+      <p>
+        색상을 고르시오 : {' '}
+        <select value={color} onChange={e => setColor(e.target.value)}>
+          <option value="lightcoral">라이트코랄</option>
+          <option value="midnightblue">미드나잇블루</option>
+          <option value="tomato">토마토</option>
+          <option value="rebeccapurple">레베카퍼플</option>
+        </select>
+      </p>
+      <Clock color={color} time={time.toLocaleTimeString()} />
+    </div>
   );
 }
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export function getImageUrl(person) {
-  return (
-    'http://i.imgur.com/' +
-    person.imageId +
-    's.jpg'
-  );
-}
